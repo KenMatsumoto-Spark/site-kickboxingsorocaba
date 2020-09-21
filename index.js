cc10c4193c4bd2daedeaf0e861dc89484225ab60
Ken Matsumoto <h264matsumoto@gmail.com>
2020-09-21 11:47:58 -0300

    email done

--- index.js
+++ index.js
@@ -18,48 +18,26 @@ function validateForm() {
   }
   var email =  document.getElementById('email').value;
   if (email == "") {
-      document.querySelector('.status').innerHTML = "Email cannot be empty";
+      document.querySelector('.status').innerHTML = "Email não pode estar vazio";
       return false;
   } else {
       var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
       if(!re.test(email)){
-          document.querySelector('.status').innerHTML = "Email format invalid";
+          document.querySelector('.status').innerHTML = "Formato do e-mail inválido";
           return false;
       }
   }
   var subject =  document.getElementById('subject').value;
   if (subject == "") {
-      document.querySelector('.status').innerHTML = "Subject cannot be empty";
+      document.querySelector('.status').innerHTML = "Assunto não pode estar vazio";
       return false;
   }
   var message =  document.getElementById('message').value;
   if (message == "") {
-      document.querySelector('.status').innerHTML = "Message cannot be empty";
+      document.querySelector('.status').innerHTML = "Mensagem não pode estar vazio";
       return false;
   }
-  document.querySelector('.status').innerHTML = "Sending...";
 
-  formData = {
-  'name'     : $('input[name=name]').val(),
-  'email'    : $('input[name=email]').val(),
-  'subject'  : $('input[name=subject]').val(),
-  'message'  : $('textarea[name=message]').val()
-  }
+  document.getElementById('contact-form').submit();
 
-  $.ajax({
-  url : "mail.php",
-  type: "POST",
-  data : formData,
-  success: function(data, textStatus, jqXHR)
-  {
-
-  $('#status').text(data.message);
-  if (data.code) //If mail was sent successfully, reset the form.
-  $('#contact-form').closest('form').find("input[type=text], textarea").val("");
-  },
-  error: function (jqXHR, textStatus, errorThrown)
-  {
-  $('#status').text(jqXHR);
-  }
-  });
 }
--- mail.php
+++ mail.php
@@ -8,7 +8,7 @@ $message = $_POST['message'];
 if(isset( $_POST['subject']))
 $subject = $_POST['subject'];
 
-$content="From: $name \n Email: $email \n Message: $message";
+$content="From: $name \nEmail: $email \nMessage: $message";
 $recipient = "h264matsumoto@gmail.com";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $content, $mailheader) or die("Error!");
